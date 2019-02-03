328626b897fad013225e937f3ab07c5cf313c37e
missxa <aliona.kharchenko@gmail.com>
2019-02-03 06:02:02 +0100

    reading separated audio

--- roboy_speech_recognition/scripts/stt_server.py
+++ roboy_speech_recognition/scripts/stt_server.py
@@ -10,13 +10,14 @@ import signal
 import pyaudio
 import traceback
 import pdb
+import socket
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(abs_path, "..", "..", "common"))
 
 from bing_voice import *
-from roboy_communication_cognition.srv import RecognizeSpeech
-from roboy_communication_control.msg import ControlLeds
+from roboy_cognition_msgs.srv import RecognizeSpeech
+from roboy_control_msgs.msg import ControlLeds
 from std_msgs.msg import Empty
 
 
@@ -25,27 +26,31 @@ BING_KEY = ''
 
 def stt_with_vad(bing, language):
 
+    global conn
     FORMAT = pyaudio.paInt16
-    CHANNELS = 1
+    CHANNELS = 4
     RATE = 16000
-    CHUNK_DURATION_MS = 30  # supports 10, 20 and 30 (ms)
+    CHUNK_DURATION_MS = 20  # supports 10, 20 and 30 (ms)
     PADDING_DURATION_MS = 1000
     CHUNK_SIZE = int(RATE * CHUNK_DURATION_MS / 1000)
     CHUNK_BYTES = CHUNK_SIZE * 2
     NUM_PADDING_CHUNKS = int(PADDING_DURATION_MS / CHUNK_DURATION_MS)
     NUM_WINDOW_CHUNKS = int(240 / CHUNK_DURATION_MS)
 
+
+
+
     vad = webrtcvad.Vad(2)
     # bing = BingVoice(BING_KEY)
 
-    pa = pyaudio.PyAudio()
-    stream = pa.open(format=FORMAT,
-                               channels=CHANNELS,
-                               rate=RATE,
-                               input=True,
-                               start=False,
-                               # input_device_index=2,
-                               frames_per_buffer=CHUNK_SIZE)
+    # pa = pyaudio.PyAudio()
+    # stream = pa.open(format=FORMAT,
+    #                            channels=CHANNELS,
+    #                            rate=RATE,
+    #                            input=True,
+    #                            start=False,
+    #                            # input_device_index=2,
+    #                            frames_per_buffer=CHUNK_SIZE)
 
 
     got_a_sentence = False
@@ -69,37 +74,51 @@ def stt_with_vad(bing, language):
         buffer_in = ''
 
         print("* recording")
-        stream.start_stream()
+        # stream.start_stream()
+
         while not got_a_sentence: #and not leave:
-            chunk = stream.read(CHUNK_SIZE)
-            active = vad.is_speech(chunk, RATE)
-            sys.stdout.write('1' if active else '0')
-            ring_buffer_flags[ring_buffer_index] = 1 if active else 0
-            ring_buffer_index += 1
-            ring_buffer_index %= NUM_WINDOW_CHUNKS
-            if not triggered:
-                ring_buffer.append(chunk)
-                num_voiced = sum(ring_buffer_flags)
-                if num_voiced > 0.5 * NUM_WINDOW_CHUNKS:
-                    sys.stdout.write('+')
-                    triggered = True
-                    voiced_frames.extend(ring_buffer)
-                    ring_buffer.clear()
-            else:
-                voiced_frames.append(chunk)
-                ring_buffer.append(chunk)
-                num_unvoiced = NUM_WINDOW_CHUNKS - sum(ring_buffer_flags)
-                if num_unvoiced > 0.9 * NUM_WINDOW_CHUNKS:
-                    sys.stdout.write('-')
-                    triggered = False
-                    got_a_sentence = True
+            # chunk = stream.read(CHUNK_SIZE)
+            # print("jhk")
+            data = conn.recv(4*CHUNK_SIZE)
+            chunk = np.frombuffer(data, dtype=np.int16)
+            channel0 = chunk[0::4]
+            chunk = channel0.tobytes()
+            if np.count_nonzero(channel0) > 1:
+                # pdb.set_trace()
+                active = False
+                try:
+                    active = vad.is_speech(channel0.tobytes(), RATE)
+
+                    sys.stdout.write('1' if active else '0')
+                    ring_buffer_flags[ring_buffer_index] = 1 if active else 0
+                    ring_buffer_index += 1
+                    ring_buffer_index %= NUM_WINDOW_CHUNKS
+                except:
+                    pass
+                if not triggered:
+                    ring_buffer.append(chunk)
+                    num_voiced = sum(ring_buffer_flags)
+                    if num_voiced > 0.5 * NUM_WINDOW_CHUNKS:
+                        sys.stdout.write('+')
+                        triggered = True
+                        voiced_frames.extend(ring_buffer)
+                        ring_buffer.clear()
+                else:
+                    voiced_frames.append(chunk)
+                    ring_buffer.append(chunk)
+                    num_unvoiced = NUM_WINDOW_CHUNKS - sum(ring_buffer_flags)
+                    if num_unvoiced > 0.9 * NUM_WINDOW_CHUNKS:
+                        sys.stdout.write('-')
+                        triggered = False
+                        got_a_sentence = True
 
             sys.stdout.flush()
 
         sys.stdout.write('\n')
         data = b''.join(voiced_frames)
+        print("Chunk size: " + str(sys.getsizeof(data)) + " bytes")
 
-        stream.stop_stream()
+        # stream.stop_stream()
         print("* done recording")
 
         # recognize speech using Microsoft Bing Voice Recognition
@@ -108,7 +127,7 @@ def stt_with_vad(bing, language):
             text = bing.recognize(data, language=language)
             # pdb.set_trace()
             print('Bing:' + text.encode('utf-8'))
-            stream.close()
+            # stream.close()
             return text
         except UnknownValueError:
             traceback.print_exc()
@@ -118,7 +137,7 @@ def stt_with_vad(bing, language):
 
         got_a_sentence = False
 
-    stream.close()
+    # stream.close()
     return text
 
 def stt_subprocess(q):
@@ -170,8 +189,20 @@ def stt_server():
     global bing
     bing = BingVoice(BING_KEY)
 
+    HOST = '0.0.0.0'
+    TCP_PORT = 10002
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, TCP_PORT))
+    s.listen(1)
+    global conn
+    conn, addr = s.accept()
+    print("Odas connected")
+
     print "Ready to recognise speech."
     rospy.spin()
+
+    conn.close()
 
 if __name__ == '__main__':
 	stt_server()
