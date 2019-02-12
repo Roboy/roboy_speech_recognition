843b91092d8f072d4bf6db3bde086dac4d26a140
missxa <aliona.kharchenko@gmail.com>
2019-02-12 04:17:58 +0100

    parallel recognition with odas

--- roboy_speech_recognition/scripts/sr_odas_src.py
+++ roboy_speech_recognition/scripts/sr_odas_src.py
@@ -2,98 +2,74 @@ import speech_recognition as sr
 import socket
 import threading
 import numpy as np
-# import pyaudio
 import pdb
 import io
 import time
+from monotonic import monotonic
 
-# import asyncio
 class Odas(sr.AudioSource):
+    class AudioSource(sr.AudioSource):
+        def __init__(self, name, sample_rate, chunk_size=1024):
+            self.SAMPLE_WIDTH = 2
+            self.SAMPLE_RATE = sample_rate
+            self.CHUNK = chunk_size
+            self.stream = Odas.BytesLoop()
+            self.record = False
+            self.name = name
 
-    def __init__(self, port, host='0.0.0.0', channel=0, chunk_size=1024, sample_rate=16000):
+        def __enter__(self):
+            # assert self.stream is None, "This audio source is already inside a context manager"
+            self.record = True
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.stream = None
+            self.record = False
+
+    def __init__(self, port, host='0.0.0.0', sample_rate=16000, chunk_size=1024):
         # self.format = pyaudio.paInt16
         self.SAMPLE_WIDTH = 2#pyaudio.get_sample_size(self.format)  # size of each sample
         self.SAMPLE_RATE = sample_rate  # sampling rate in Hertz
         self.CHUNK = chunk_size
+        self.CHANNELS = 4
 
-        self.audio = None
-        self.stream = None
-        self.pyaudio_stream = None
+        self.stream = Odas.BytesLoop()
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind((host, port))
         self.s.listen(1)
         self.conn, self.addr = self.s.accept()
         print("Odas connected")
-        self.record = True
 
-
-        d = threading.Thread(target=self.write_to_stream)
+        d = threading.Thread(target=self.write_to_streams)
         self.lock = threading.RLock()
         d.setDaemon(True)
         d.start()
 
-        self.stream = Odas.BytesLoop()
-        self.channels = [sr.AudioSource]
+        self.channels = []
+        for i in range(self.CHANNELS):
+            self.channels.append(self.AudioSource(name="odas_%i"%i, \
+                            sample_rate=sample_rate, chunk_size=chunk_size))
 
-
-    def write_to_stream(self):#, reader, writer):
-        print("Started deamon")
+    def write_to_streams(self):
+        print("Started odas deamon")
         while True:
             data = self.conn.recv(4*self.CHUNK)
-            # self.lock.acquire()
-            # print("got lock")
-            if self.record:
-                # print("oh")
-
-                try:
-                    data = np.frombuffer(data, dtype=np.int16)
-                    # if np.count_nonzero(data) > 0:
-                        # pdb.set_trace()
-                        # print("this is not a zero")
-                        # energy = audioop.rms(data, self.SAMPLE_WIDTH)
-                        # if energy > 0:
-                        #     print ("energy is not zero")
-                    # for i in range(4):
-                    #     data = data[i:4].tobytes()
-                    #     energy = audioop.rms(data, source.SAMPLE_WIDTH)
-                    #     if energy >0:
-                    #         print("in channel possitive energy " + str(i))
-                    data = data[0::4].tobytes()
-                    self.stream.write(data)
-                    # print("yay")
-                except:
-                    pass
-            # self.lock.release()
-                # print("released lock")
+            # if self.record:
+            data = np.frombuffer(data, dtype=np.int16)
+            #     self.stream.write(data[0::4].tobytes())
+            for i in range(self.CHANNELS):
+                if self.channels[i].record:
+                    self.channels[i].stream.write(data[i::4].tobytes())
 
     def __enter__(self):
         # assert self.stream is None, "This audio source is already inside a context manager"
-        # self.audio = pyaudio.PyAudio()
-        try:
-            # pdb.set_trace()
-            # self.lock.acquire()
-            self.record = True
-            # self.lock.release()
-
-            # self.pyaudio_stream = self.audio.open(
-            #     channels=1, format=self.format,
-            #     rate=self.SAMPLE_RATE, frames_per_buffer=self.CHUNK, output=True, input=True
-            # )
-            # self.stream = Odas.MicrophoneStream(self.pyaudio_stream)
-
-
-        except Exception:
-            print("exception in enter")
-            # self.audio.terminate()
+        self.record = True
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-            # self.lock.acquire()
-            self.stream = None
-            self.record = False
-            # self.lock.release()
-            # self.audio.terminate()
+        self.stream = None
+        self.record = False
 
     class BytesLoop:
         def __init__(self, s=b''):
@@ -104,61 +80,39 @@ class Odas(sr.AudioSource):
             # print("read %i"%n)
             while len(self.buffer) < n:
                 pass
-            self.lock.acquire()
+            # self.lock.acquire()
 
             chunk = self.buffer[:n]
             self.buffer = self.buffer[n:]
-            self.lock.release()
+            # self.lock.release()
             return chunk
 
         def write(self, s):
             # print("write %i"%len(s))
-            self.lock.acquire()
+            # self.lock.acquire()
             self.buffer += s
-            self.lock.release()
-
-    class MicrophoneStream(object):
-        def __init__(self, pyaudio_stream):
-            self.pyaudio_stream = pyaudio_stream
-
-        def read(self, size):
-            return self.pyaudio_stream.read(size)#, exception_on_overflow=False)
-
-        def close(self):
-            try:
-                # sometimes, if the stream isn't stopped, closing the stream throws an exception
-                if not self.pyaudio_stream.is_stopped():
-                    self.pyaudio_stream.stop_stream()
-            finally:
-                self.pyaudio_stream.close()
+            # self.lock.release()
 
 
-r = sr.Recognizer()
 
-# loop = asyncio.get_event_loop()
-
-with Odas(10002) as source:
-# with sr.AudioFile('/home/missxa/workspace/Roboy/src/roboy_speech_recognition/roboy_speech_recognition/scripts/speech_recognition/examples/english.wav') as source:
-    # coro = asyncio.start_server(source.write_to_stream, host, port, loop=loop)
-    # server = loop.run_until_complete(coro)
-    # time.sleep(3)
-    # while True:
-    #     print(len(source.stream.buffer))
-    audio = r.listen(source)
 
 BING_KEY = ""  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print(r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
 
+r = sr.Recognizer()
+o = Odas(port=10002, chunk_size=2048)
+def listener(source):
+    with source as source:
+        audio = r.listen(source=source)
+        try:
+            print("%s: "%source.name + r.recognize_bing(audio, key=BING_KEY))
+        except sr.UnknownValueError:
+            print("Microsoft Bing Voice Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
 
-# loop.run_forever()
-# print("doing some async stuff")
-# o = Odas(10002)
-# o = o.__enter__()
-# time.sleep(4)
-# pdb.set_trace()
-# o.__exit__(None, None, None)
+listeners = []
+for i in range(4):
+    listeners.append(threading.Thread(target=listener, args = (o.channels[i],)))
+
+for l in listeners:
+    l.start()
