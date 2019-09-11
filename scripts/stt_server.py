87641718fdd0519de3a2e8aa3cc646c8ccbd2a1c
missxa <aliona.kharchenko@gmail.com>
2019-09-11 19:55:27 +0100

    creator mics by default

--- scripts/stt_server.py
+++ scripts/stt_server.py
@@ -92,7 +92,7 @@ class ResumableMicrophoneStream:
             rate=self._rate,
             input=True,
             frames_per_buffer=self._chunk_size,
-            input_device_index=8,
+            input_device_index=3,
             # Run the audio stream asynchronously to fill the buffer object.
             # This is necessary so that the input device's buffer doesn't
             # overflow while the calling thread makes network requests, etc.
